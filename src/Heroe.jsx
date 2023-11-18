40f36f4264cabffbff702afa5c308368c6ad99f8
kofla10 <juank_milo94@hotmail.com>
2023-11-17 23:04:02 -0500

    start

---
+++ src/Heroe.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Heroe = () => {
+  return (
+    <h1>Heroe</h1>
+  )
+}
---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './style.css'
+import { Heroe } from './Heroe'
+import { BrowserRouter } from 'react-router-dom'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Heroe/>
+    </BrowserRouter>
+  </React.StrictMode>,
+)
